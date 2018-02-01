0d4fcc96a368d9992bed9d6183b6b5c7fc8934e9
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-02-02 00:40:07 +0900

    Draw Arc

--- Core Graphics/View/CounterView.swift
+++ Core Graphics/View/CounterView.swift
@@ -8,14 +8,34 @@
 
 import UIKit
 
-class CounterView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+@IBDesignable class CounterView: UIView {
+    
+    private struct Constants {
+        static let numberOfGlasses = 8
+        static let lineWidth:CGFloat = 5.0
+        static let arcWidth:CGFloat = 76
+    
+        static var halfOfLineWidth: CGFloat {
+            return lineWidth / 2
+        }
     }
-    */
-
+    
+    @IBInspectable var counter: Int = 5
+    @IBInspectable var outlineColor: UIColor = UIColor.blue
+    @IBInspectable var counterColor: UIColor = UIColor.orange
+    
+    override func draw(_ rect: CGRect) {
+        let center = CGPoint(x: bounds.width / 2, y: bounds.height / 2)
+        let radius: CGFloat = max(bounds.width, bounds.height)
+        
+        let startAngle: CGFloat = 3 * .pi / 4
+        let endAngle: CGFloat = .pi / 4
+        
+        let path = UIBezierPath(arcCenter: center, radius: radius/2 - Constants.arcWidth/2, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+        
+        path.lineWidth = Constants.arcWidth
+        counterColor.setStroke()
+        path.stroke()
+    }
+    
 }
