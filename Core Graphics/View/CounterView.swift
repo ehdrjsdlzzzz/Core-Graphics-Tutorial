8b92cc26ce1fc7b3f9491bcbdb43c1b28a61048e
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-02-03 00:21:14 +0900

    change variable name

--- Core Graphics/View/CounterView.swift
+++ Core Graphics/View/CounterView.swift
@@ -43,9 +43,9 @@ import UIKit
         counterColor.setStroke()
         path.stroke()
         
-        let angleDifference: CGFloat = 2 * .pi - startAngle + endAngle
-        let arcLengthPerGlass = angleDifference / CGFloat(Constants.numberOfGlasses)
-        let outlineEndAngle = arcLengthPerGlass * CGFloat(counter) + startAngle
+        let arcAngle: CGFloat = 2 * .pi - startAngle + endAngle
+        let arcAnglePerGlass = arcAngle / CGFloat(Constants.numberOfGlasses)
+        let outlineEndAngle = arcAnglePerGlass * CGFloat(counter) + startAngle
         
         let outlinePath = UIBezierPath(arcCenter: center, radius: bounds.width/2 - Constants.halfOfLineWidth, startAngle: startAngle, endAngle: outlineEndAngle, clockwise: true)
         outlinePath.addArc(withCenter: center, radius: bounds.width/2 - Constants.arcWidth + Constants.halfOfLineWidth, startAngle: outlineEndAngle, endAngle: startAngle, clockwise: false)
